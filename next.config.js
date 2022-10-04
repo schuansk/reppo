878c025670c0a3ca57ccc89cd8749d2161ec74a0
schuansk <schuansk@hotmail.com.br>
2022-10-04 20:51:21 -0300

    redirect to home

--- next.config.js
+++ next.config.js
@@ -1,7 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/home',
+        permanent: true,
+        basePath: false,
+      },
+    ];
+  },
   reactStrictMode: true,
   swcMinify: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
